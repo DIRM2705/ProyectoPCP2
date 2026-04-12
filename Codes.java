941304ddaf5a22144f2fa00df5691040895deb52
Alan Ortiz <108891954+Okioh@users.noreply.github.com>
2026-04-12 00:01:55 -0600

    Codigo Nuevo
    Se creo un nuevo codigo "11" para el servidor y la lista de archivos

--- Codes.java
+++ Codes.java
@@ -9,4 +9,17 @@ public class Codes
     public static final int UPDATE_DOC = 4;
     public static final int ERROR = 5;
     public static final int NEW_CLIENT = 6;
+
+    // --- NUEVOS CÓDIGOS PARA P2P ---
+    // El cliente avisa al servidor: "Guarde el fragmento N de este documento"
+    public static final int REPORT_CHUNK = 7; 
+    
+    // El cliente pregunta: "¿Dónde están los fragmentos de este documento?"
+    public static final int REQUEST_LOCATIONS = 8; 
+    
+    // El servidor responde con el mapa de ubicaciones
+    public static final int LOCATIONS_RESPONSE = 9; 
+    // Comando para el inventario en el servidor 
+    public static final int INVENTORY =11;
 }
+
