7efeaf53caa02fe5028ce1bd9a822da3ba8218a9
Alan Ortiz <108891954+Okioh@users.noreply.github.com>
2026-05-06 14:17:18 -0600

    Pa que no ande chillando bruno

--- Server/Main.java
+++ Server/Main.java
@@ -8,7 +8,7 @@ public class Main
 {
     public static final Set<String> clientesConectados = new CopyOnWriteArraySet<>();
     public static int connections = 0; //Número máximo de usuarios que pueden abrir un documento, se asigna a 255 para que el número de usuarios pueda ser representado en un byte
-    public static final BroadcastConn broadcastMessaging = new BroadcastConn("192.168.1.255", 1234);
+    public static final BroadcastConn broadcastMessaging = new BroadcastConn("172.31.15.255", 1234);
     public static TablaDocs tablaDocs = null;
     public static int docCounter = 0; //Contador para asignar un número único a cada documento creado, se incrementa cada vez que se crea un nuevo documento
     public static void main(String[] args)
